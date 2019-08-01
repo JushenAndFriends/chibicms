646cac53e648ebf2e7262b2bcd8e2b438fa7383e
zhengwei <zhenghaku@gmail.com>
2019-08-01 18:45:11 +0800

    added hust projecView, added some example, construct the basic content layout

---
+++ Code/ChibiCmsWeb/Helpers/ContentHtmlHelper.cs
@@ -0,0 +1,33 @@
+﻿using Jushen.ChibiCms.ChibiContent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ChibiCmsWeb.Helpers
+{
+    public static class ContentHtmlHelper
+    {
+        public static string GetLinkToContent(ContentMeta meta)
+        {
+            switch (meta.ContentType)
+            {
+                case ContentMeta.TypeContent:
+                    return string.Format("/contents{0}", meta.WebPath);
+                case ContentMeta.TypeDirectory:
+                    return string.Format("/index/index{0}", meta.WebPath);
+
+                default:
+                    return "#";
+            }
+            
+        }
+
+        public static string RemoveScript(this string input)
+        {
+            Regex rRemScript = new Regex(@"<script[^>]*>[\s\S]*?</script>");
+            return rRemScript.Replace(input, "");
+        }
+    }
+}
---
+++ Code/ChibiCmsWeb/Models/ProjectInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ChibiCmsWeb.Models
+{
+    public class ProjectInfo
+    {
+        public string TeamName { get; set; }
+        public string Abstract { get; set; }
+        public string Teacher { get; set; }
+        public string ProjectLink { get; set; }
+        public List<string> TeamMember { get; set; }
+        public List<string> Posters { get; set; }
+        /// <summary>
+        /// only support youku
+        /// </summary>
+        public string EmbeddedVideo { get; set; }
+    }
+}
